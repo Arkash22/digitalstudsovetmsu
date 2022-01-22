db071859ee5bb9b3bed43e5a35f8178c606e1133
Arkash22 <66271450+Arkash22@users.noreply.github.com>
2022-01-22 23:51:58 +0300

    Update settings.py

--- BOT_DVI/settings.py
+++ BOT_DVI/settings.py
@@ -1,9 +1,8 @@
 import psycopg2
 import os, sys
 
-conn = psycopg2.connect(dbname='msu_dvi_bot', user='mrkrap',
-                        password='5170', host='89.108.64.97')
+conn = psycopg2.connect(dbname='---', user='user',
+                        password='-', host='--.---.--.--')
 cursor = conn.cursor()  # обращаться будем через курсор
 
-BotToken = '1700612569:AAGdmn_bIN0lxRY5AOB_ns3EV4MCJGeHpc8' # @test_drafer_bot
-# pic_folder = 'C:/Users/Mark/OneDrive - msu.ru/Рабочий стол/drafter_bot/photo/'
+BotToken = '' # @msu_dvi_bot
